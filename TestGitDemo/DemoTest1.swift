d5b70d24f42db8ac20a566a999914ca59445e2e4
nikunjramimobi <Mohak@localhost.local>
2016-08-16 18:56:46 +0530

    added demo test swift file
    just demo

---
+++ TestGitDemo/DemoTest1.swift
@@ -0,0 +1,40 @@
+//
+//  DemoTest1.swift
+//  TestGitDemo
+//
+//  Created by Raviraj Jadeja on 16/08/16.
+//  Copyright © 2016 Mag_Mini_1. All rights reserved.
+//
+
+/*
+ 
+ ******* Test Sohan 1
+ 
+ */
+import UIKit
+
+class DemoTest1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
